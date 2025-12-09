d4039912397f13d8da04c8ec8517df624d0481f6
ankit-7i <ankitrout513151979@gmail.com>
2025-12-09 11:21:03 +0530

    SpringCore

---
+++ springcore_collectiondemo_2/src/main/java/com/example/springcore_collectiondemo_2/SpringcoreCollectiondemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.springcore_collectiondemo_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoreCollectiondemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoreCollectiondemo2Application.class, args);
+	}
+
+}
---
+++ springcore_collectiondemo_2/src/test/java/com/example/springcore_collectiondemo_2/SpringcoreCollectiondemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcore_collectiondemo_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreCollectiondemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
