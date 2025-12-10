4ec25a2e3645e9f142f95fd3a9bd18d86181a599
ankit-7i <ankitrout513151979@gmail.com>
2025-12-10 11:20:50 +0530

    SpringCore

--- springcoredemo/src/main/java/com/ankit/springlab/springcoredemo/Test.java
+++ springcoredemo/src/main/java/com/ankit/springlab/springcoredemo/Test.java
@@ -7,11 +7,13 @@ public class Test {
 
     public static void main(String[] args) {
 
+        @SuppressWarnings("resource")
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("bean.xml");
 
         Student s1 = context.getBean("student1", Student.class);
 
         System.out.println(s1);
+        
     }
 }
---
+++ springcoredemo_3/src/main/java/com/example/springcoredemo_3/Springcoredemo3Application.java
@@ -0,0 +1,13 @@
+package com.example.springcoredemo_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcoredemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springcoredemo3Application.class, args);
+	}
+
+}
