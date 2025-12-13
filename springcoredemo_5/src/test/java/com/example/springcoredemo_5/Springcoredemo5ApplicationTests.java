00aaa6f13d5c386a1dc0304b0237d8836273b401
ankit-7i <ankitrout513151979@gmail.com>
2025-12-13 16:44:13 +0530

    SpringCore

---
+++ springcoredemo_5/src/test/java/com/example/springcoredemo_5/Springcoredemo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcoredemo_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springcoredemo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
