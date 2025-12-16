5605e6cc77636fec485ba8704a24e5c7ed355d7f
ankit-7i <ankitrout513151979@gmail.com>
2025-12-16 16:44:10 +0530

    SpringCore

---
+++ springcorejdbcdemo_6/src/test/java/com/example/springcorejdbcdemo_6/Springcorejdbcdemo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcorejdbcdemo_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springcorejdbcdemo6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
