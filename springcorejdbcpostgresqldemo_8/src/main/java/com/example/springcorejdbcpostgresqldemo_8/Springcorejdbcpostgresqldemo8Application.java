cb2a70fff66497fc6037fa1c7d94b67f3db0b8fb
ankit-7i <ankitrout513151979@gmail.com>
2025-12-17 16:34:35 +0530

    SpringCore

---
+++ springcorejdbcpostgresqldemo_8/src/main/java/com/example/springcorejdbcpostgresqldemo_8/Springcorejdbcpostgresqldemo8Application.java
@@ -0,0 +1,13 @@
+package com.example.springcorejdbcpostgresqldemo_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcorejdbcpostgresqldemo8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springcorejdbcpostgresqldemo8Application.class, args);
+	}
+
+}
