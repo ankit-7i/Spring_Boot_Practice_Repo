e1af0d6b78f688f480ee4a7e01c8c2cc438063e8
ankit-7i <ankitrout513151979@gmail.com>
2025-12-19 23:42:32 +0530

    SpringCore

---
+++ springcorebeanlifecycledemo_11/src/main/java/com/beanlifecycle/Springcorebeanlifecycledemo11Application.java
@@ -0,0 +1,13 @@
+package com.beanlifecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcorebeanlifecycledemo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springcorebeanlifecycledemo11Application.class, args);
+	}
+
+}
---
+++ springcorebeanlifecycledemo_11/src/test/java/com/beanlifecycle/Springcorebeanlifecycledemo11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beanlifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springcorebeanlifecycledemo11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
