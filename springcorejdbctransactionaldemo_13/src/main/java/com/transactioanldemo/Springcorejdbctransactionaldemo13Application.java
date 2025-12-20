99c47a88b6d088e7abcde5128c389cccb34ecfb9
ankit-7i <ankitrout513151979@gmail.com>
2025-12-20 16:52:30 +0530

    SpringCore

---
+++ springcorejdbctransactionaldemo_13/src/main/java/com/transactioanldemo/Springcorejdbctransactionaldemo13Application.java
@@ -0,0 +1,13 @@
+package com.transactioanldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcorejdbctransactionaldemo13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springcorejdbctransactionaldemo13Application.class, args);
+	}
+
+}
---
+++ springcorejdbctransactionaldemo_13/src/test/java/com/transactioanldemo/Springcorejdbctransactionaldemo13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transactioanldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springcorejdbctransactionaldemo13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
