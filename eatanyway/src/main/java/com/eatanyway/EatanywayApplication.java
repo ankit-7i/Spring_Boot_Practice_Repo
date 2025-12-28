e9e43da7c35c35904950cf9eef2fa735b3390663
ankit-7i <ankitrout513151979@gmail.com>
2025-12-28 20:29:48 +0530

    SpringCore

---
+++ eatanyway/src/main/java/com/eatanyway/EatanywayApplication.java
@@ -0,0 +1,13 @@
+package com.eatanyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatanywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatanywayApplication.class, args);
+	}
+
+}
---
+++ eatanyway/src/test/java/com/eatanyway/EatanywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatanyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatanywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
